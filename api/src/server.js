694e436f168d8c9098def329a3c93c727dd0c6a5
Gregoire <greg.thomas4205@gmail.com>
2025-02-21 01:22:36 +0100

    creation-front

---
+++ api/src/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: ".env" });
+const pkmnRoutes = require("./routes/pkmn.routes");
+const userRoutes = require("./routes/user.routes");
+const authRoutes = require("./routes/auth.routes");
+const trainerRoutes = require("./routes/trainer.routes");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+
+const app = express();
+const port = 3000;
+
+// Middlewares globaux
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connexion à MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/td")
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((err) => console.log(err));
+
+// Routes
+app.use("/api/pkmn", pkmnRoutes);
+app.use("/api", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/trainer", trainerRoutes);
+
+// Charger la doc Swagger
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Lancement du serveur
+app.listen(port, () => {
+  console.log(`🚀 Serveur lancé sur http://localhost:${port}`);
+});
